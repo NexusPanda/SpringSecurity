4a07e2735e909436f01b606414859f87507448b6
NexusPanda <157189888+NexusPanda@users.noreply.github.com>
2025-05-24 22:04:56 +0530

    Add files via upload

---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/SimpleController.java
@@ -0,0 +1,27 @@
+package com.example.SpringSecurity;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String userEndpoint(){
+        return "Hello User !!!";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminEndpoint(){
+        return "Hello Admin !!!";
+    }
+}
