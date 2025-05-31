5c41e201987bc76a95a311fdfc0818979d752a79
NexusPanda <157189888+NexusPanda@users.noreply.github.com>
2025-05-31 22:07:12 +0530

    Sign-In flow JWT

--- SpringSecurity/src/main/java/com/example/SpringSecurity/SimpleController.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/SimpleController.java
@@ -1,12 +1,35 @@
 package com.example.SpringSecurity;
 
+import com.example.SpringSecurity.JWT.JwtUtils;
+import com.example.SpringSecurity.JWT.LoginRequest;
+import com.example.SpringSecurity.JWT.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class SimpleController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @GetMapping("/hello")
     public String sayHello(){
@@ -23,5 +46,39 @@ public class SimpleController {
     @PreAuthorize("hasRole('ADMIN')")
     public String adminEndpoint(){
         return "Hello Admin !!!";
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest){
+        Authentication authentication;
+        try {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                    )
+            );
+
+        } catch (AuthenticationException e){
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "Bad Credentials");
+            map.put("status", false);
+
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String jwtToken = jwtUtils.getTokenFromUserName(userDetails);
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .toList();
+
+        LoginResponse loginResponse = new LoginResponse(
+                userDetails.getUsername(), jwtToken, roles
+        );
+
+        return ResponseEntity.ok(loginResponse);
     }
 }
